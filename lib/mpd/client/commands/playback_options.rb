b5a692c499f13872fb988e3f2e6259ecf04a3167
Dmitriy Non <non.dmitriy@gmail.com>
2017-12-15 11:23:01 +0300

    MPD::Commands::SetVolume

--- lib/mpd/client/commands/playback_options.rb
+++ lib/mpd/client/commands/playback_options.rb
@@ -3,10 +3,6 @@ module MPD
     module Commands
       # https://www.musicpd.org/doc/protocol/playback_option_commands.html
       module PlaybackOptions
-        def volume(val)
-          execute "setvol #{val}"
-        end
-
         def change_volume_by(val)
           execute("volume #{val}")
         end
