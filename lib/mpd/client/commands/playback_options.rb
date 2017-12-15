e3f9066026f4e361703cdb7fe34407eb6f8ddb95
Dmitriy Non <non.dmitriy@gmail.com>
2017-12-15 11:14:54 +0300

    create commands for options

--- lib/mpd/client/commands/playback_options.rb
+++ lib/mpd/client/commands/playback_options.rb
@@ -3,26 +3,6 @@ module MPD
     module Commands
       # https://www.musicpd.org/doc/protocol/playback_option_commands.html
       module PlaybackOptions
-        def consume(state)
-          execute "consume #{state ? 1 : 0}"
-        end
-
-        def crossfade(state)
-          execute "crossfade #{state ? 1 : 0}"
-        end
-
-        def random(state)
-          execute "random #{state ? 1 : 0}"
-        end
-
-        def repeat(state)
-          execute "repeat #{state ? 1 : 0}"
-        end
-
-        def single(state)
-          execute "single #{state ? 1 : 0}"
-        end
-
         def volume(val)
           execute "setvol #{val}"
         end
--- lib/mpd/commands.rb
+++ lib/mpd/commands.rb
@@ -22,5 +22,22 @@ module MPD
     define_trivial_command(:Stop, 'stop')
     define_trivial_command(:Previous, 'previous')
     define_trivial_command(:Next, 'next')
+
+    #
+    # To define commands like "random <1/0>"
+    #
+    def self.define_option_command(class_name, command)
+      klass = Class.new(::MPD::Commands::Abstract) do
+        define_method :execute do |state|
+          super(command + state ? ' 1' : ' 0')
+        end
+      end
+
+      const_set(class_name, klass)
+    end
+
+    %w[Consume Crossfade Random Repeat Single].each do |class_name|
+      define_option_command(class_name, class_name.downcase)
+    end
   end
 end
