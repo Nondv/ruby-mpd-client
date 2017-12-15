3f93049dbd9c6d1110de309728014fa4db8c036c
Dmitriy Non <non.dmitriy@gmail.com>
2017-12-15 13:22:03 +0300

    make MPD::Commands::CurrentPlaylistRemove except position or id

--- lib/mpd/commands/current_playlist_remove.rb
+++ lib/mpd/commands/current_playlist_remove.rb
@@ -4,13 +4,16 @@ module MPD
   module Commands
     #
     # Removes songs from current playlist
+    # by id or position/range
     #
     class CurrentPlaylistRemove < Abstract
       #
-      # `songs` is position (as int) or range
+      # `position` is int or range
+      # `id` is int song id
       #
-      def execute(songs)
-        super("delete #{resolve_range(songs)}")
+      def execute(position: nil, id: nil)
+        raise(ArgumentError) if position && id || !position && !id
+        exec_command(id ? "deleteid #{id}" : "delete #{resolve_range(position)}")
       end
     end
   end
