f48bd2d53fc514192b79caa061c7dbe6d31abacb
Dmitriy Non <non.dmitriy@gmail.com>
2017-12-15 07:54:26 +0300

    replace MPD::Client::Commands::PlaybackControl#play with Commands::Play

--- lib/mpd/client/commands/playback_control.rb
+++ lib/mpd/client/commands/playback_control.rb
@@ -3,18 +3,6 @@ module MPD
     module Commands
       # https://www.musicpd.org/doc/protocol/playback_commands.html
       module PlaybackControl
-        def play(position: nil, id: nil)
-          raise(ArgumentError, 'too many arguments') if position && id
-
-          if position
-            execute "play #{position}"
-          elsif id
-            execute "playid #{id}"
-          else
-            execute 'pause 0'
-          end
-        end
-
         def pause
           execute 'pause 1'
         end
