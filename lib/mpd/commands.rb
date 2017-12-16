4cea14d52a5c48b4e194aeb93fe5c263f5fa323f
Dmitriy Non <non.dmitriy@gmail.com>
2017-12-16 17:13:58 +0300

    reformat code in Commands

--- lib/mpd/commands.rb
+++ lib/mpd/commands.rb
@@ -18,11 +18,18 @@ module MPD
       const_set(class_name, klass)
     end
 
-    define_trivial_command(:Pause, 'pause 1')
-    define_trivial_command(:Stop, 'stop')
-    define_trivial_command(:Previous, 'previous')
-    define_trivial_command(:Next, 'next')
-    define_trivial_command(:CurrentPlaylistClear, 'clear')
+    #
+    # To define commands like 'save "whatever this is"'
+    #
+    def self.define_text_argument_command(class_name, command)
+      klass = Class.new(::MPD::Commands::Abstract) do
+        define_method :execute do |arg|
+          super("#{command} \"#{arg}\"")
+        end
+      end
+
+      const_set(class_name, klass)
+    end
 
     #
     # To define commands like "random <1/0>"
@@ -37,25 +44,18 @@ module MPD
       const_set(class_name, klass)
     end
 
-    %w[Consume Crossfade Random Repeat Single].each do |class_name|
-      define_option_command(class_name, class_name.downcase)
-    end
-
-    #
-    # To define commands like 'save "whatever this is"'
-    #
-    def self.define_text_argument_command(class_name, command)
-      klass = Class.new(::MPD::Commands::Abstract) do
-        define_method :execute do |arg|
-          super("#{command} \"#{arg}\"")
-        end
-      end
-
-      const_set(class_name, klass)
-    end
+    define_trivial_command(:Pause, 'pause 1')
+    define_trivial_command(:Stop, 'stop')
+    define_trivial_command(:Previous, 'previous')
+    define_trivial_command(:Next, 'next')
+    define_trivial_command(:CurrentPlaylistClear, 'clear')
 
     define_text_argument_command :PlaylistDelete, :rm
     define_text_argument_command :PlaylistSave, :save
     define_text_argument_command :PlaylistLoad, :load
+
+    %w[Consume Crossfade Random Repeat Single].each do |class_name|
+      define_option_command(class_name, class_name.downcase)
+    end
   end
 end
