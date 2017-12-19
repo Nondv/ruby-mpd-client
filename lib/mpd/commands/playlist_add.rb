d08871ed34135761c2fb9e48f3aff24f9a9fdf69
Dmitriy Non <non.dmitriy@gmail.com>
2017-12-19 13:29:56 +0300

    update doc comment on PlaylistAdd

--- lib/mpd/commands/playlist_add.rb
+++ lib/mpd/commands/playlist_add.rb
@@ -3,13 +3,12 @@ require 'mpd/commands/abstract'
 module MPD
   module Commands
     #
-    # Adds song(s) to specific playlist
-    # `songs` is an array of URIs.
+    # Adds song(s) to specific playlist.
     #
     class PlaylistAdd < Abstract
       #
-      # `name` is playlist name.
-      # `songs` is position (as int) or range.
+      # @param name [String] playlist name.
+      # @param songs [Array<String>, String] file URIs.
       #
       def execute(name, songs)
         return exec_command("playlistadd \"#{name}\" \"#{songs}\"") unless songs.is_a?(Array)
