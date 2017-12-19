26920d749acf51d3a7fb3254fc5a8742f203eac9
Dmitriy Non <non.dmitriy@gmail.com>
2017-12-19 13:36:24 +0300

    fix DSL documentation macros in Commands

--- lib/mpd/commands.rb
+++ lib/mpd/commands.rb
@@ -8,6 +8,10 @@ module MPD
     #
     # To decrease code amount
     #
+    # @!macro [attach] define_trivial_command
+    #   @!parse
+    #     # Sends "$2" to MPD.
+    #     class Commands::$1 < Commands::Abstract; end
     def self.define_trivial_command(class_name, command)
       klass = Class.new(::MPD::Commands::Abstract) do
         define_method :execute do
@@ -21,6 +25,10 @@ module MPD
     #
     # To define commands like 'save "whatever this is"'
     #
+    # @!macro [attach] define_text_argument_command
+    #   @!parse
+    #     # Sends '$2 "<ARGUMENT>"' to MPD
+    #     class Commands::$1 < Commands::Abstract; end
     def self.define_text_argument_command(class_name, command)
       klass = Class.new(::MPD::Commands::Abstract) do
         define_method :execute do |arg|
@@ -34,6 +42,13 @@ module MPD
     #
     # To define commands like "random <1/0>"
     #
+    # @!macro [attach] define_option_command
+    #   @!parse
+    #     # Sends '$2 <1/0>' to MPD
+    #     class Commands::$1 < Commands::Abstract
+    #       # @param state [Boolean]
+    #       def execute(state); end
+    #     end
     def self.define_option_command(class_name, command)
       klass = Class.new(::MPD::Commands::Abstract) do
         define_method :execute do |state|
@@ -44,32 +59,17 @@ module MPD
       const_set(class_name, klass)
     end
 
-    # @!macro [attach] define_trivial_command
-    #   @!parse
-    #     # Sends "$2" to MPD.
-    #     class Commands::$1 < Commands::Abstract; end
     define_trivial_command('Pause', 'pause 1')
     define_trivial_command('Stop', 'stop')
     define_trivial_command('Previous', 'previous')
     define_trivial_command('Next', 'next')
     define_trivial_command('CurrentPlaylistClear', 'clear')
 
-    # @!macro [attach] define_text_argument_command
-    #   @!parse
-    #     # Sends '$2 "<ARGUMENT>"' to MPD
-    #     class Commands::$1 < Commands::Abstract; end
     define_text_argument_command 'PlaylistDelete', :rm
     define_text_argument_command 'PlaylistSave', :save
     define_text_argument_command 'PlaylistLoad', :load
     define_text_argument_command 'PlaylistClear', :playlistclear
 
-    # @!macro [attach] define_option_command
-    #   @!parse
-    #     # Sends '$2 <1/0>' to MPD
-    #     class Commands::$1 < Commands::Abstract
-    #       # @param state [Boolean]
-    #       def execute(state); end
-    #     end
     define_option_command 'Consume', 'consume'
     define_option_command 'Crossfade', 'crossfade'
     define_option_command 'Random', 'random'
